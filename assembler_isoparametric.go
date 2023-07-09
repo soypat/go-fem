dd4233bde5e3bf1a9cecdda94e811ff2b7e0449c
soypat <graded.sp@gmail.com>
2023-07-08 23:51:31 -0300

    add patch test for axisymmetric elements

--- assembler_isoparametric.go
+++ assembler_isoparametric.go
@@ -121,3 +121,102 @@ func (ga *GeneralAssembler) AddIsoparametric(elemT Isoparametric, c IsoConstitut
 	ga.ksolid.Accumulate(spac)
 	return nil
 }
+
+// IsoparametricStrains calculates the strains at the integration points of an isoparametric element.
+func (ga *GeneralAssembler) IsoparametricStrains(displacements lap.Vector, elemT Isoparametric, c IsoConstituter, Nelem int, getElement func(i int) (elem []int, xC, yC r3.Vec), strainCallback func(iele int, strains []float64)) error {
+	nDisp := displacements.Len()
+	if nDisp != ga.TotalDofs() {
+		return fmt.Errorf("displacements vector length %d does not match total number of dofs %d", nDisp, ga.TotalDofs())
+	}
+	C, err := c.Constitutive()
+	if err != nil {
+		return err
+	}
+	dimC, _c := C.Dims()
+	if _c != dimC {
+		return fmt.Errorf("expected constitutive matrix to be square, got %dx%d", dimC, c)
+	}
+
+	var (
+		// Number of integration dimensions per node. Usually spatial, so 2 for 2D problems and 3 for 3D problems.
+		NdimsPerNode = len(elemT.BasisDiff(r3.Vec{})) / elemT.LenNodes()
+		// Number of dofs per node. These contain the field variables.
+		// For example, for a 2D displacement problem these are the x and y displacements, so equal to 2.
+		// For a thermal problem there is always only 1 dof per node for the temperature, regardless of the number of spatial dimensions.
+		NdofsPerNode = elemT.Dofs().Count() //
+		// Number of nodes per element.
+		NnodperElem = elemT.LenNodes()
+		// Number of dofs per element.
+		NdofperElem = NnodperElem * NdofsPerNode
+		// Element stiffness matrix. The results of integrating the element's stiffness matrix over the element's domain.
+		Ke = mat.NewDense(NdofperElem, NdofperElem, nil)
+		// number of columns in Compliance x NdofPerNode*nodesperelement
+		B = mat.NewDense(dimC, NdofperElem, nil)
+		// Differentiated form functions with respect to the integration coordinates.
+		dNxy = mat.NewDense(NdimsPerNode, NnodperElem, nil)
+		// Quadrature integration points.
+		upg, wpg = elemT.Quadrature()
+		// Compliance matrix as a Dense matrix.
+		Cd = mat.NewDense(dimC, dimC, nil)
+	)
+	if len(upg) == 0 || len(upg) != len(wpg) {
+		return fmt.Errorf("bad quadrature result from isoparametric element")
+	}
+	Cd.Copy(C)
+
+	// Calculate form functions evaluated at integration points.
+	Npg := make([]*mat.VecDense, len(upg))
+	dNpg := make([]*mat.Dense, len(upg))
+	for ipg, pg := range upg {
+		Npg[ipg] = mat.NewVecDense(NnodperElem, elemT.Basis(pg))
+		dNpg[ipg] = mat.NewDense(NdimsPerNode, NnodperElem, elemT.BasisDiff(pg))
+	}
+	// Allocate memory for auxiliary matrices.
+	jac := mat.NewDense(NdimsPerNode, NdimsPerNode, nil)
+	// pgStrain := lap.NewDenseMatrix(dimC, len(upg), nil)
+	pgStrain := mat.NewDense(dimC, len(upg), nil)
+
+	var x, y r3.Vec
+	subGetElement := func(i int) (elem []int) {
+		elem, x, y = getElement(i)
+		return elem
+	}
+	err = ga.ForEachElement(elemT, NdimsPerNode, Nelem, subGetElement, func(iele int, elemNodBacking []float64, elemDofs []int) error {
+		if x != (r3.Vec{}) || y != (r3.Vec{}) {
+			return errors.New("arbitrary constitutive orientation not implemented yet")
+		}
+		Ke.Zero()
+		elemNod := mat.NewDense(NnodperElem, NdimsPerNode, elemNodBacking)
+		for ipg := range upg {
+			dN := dNpg[ipg]
+			jac.Mul(dN, elemNod)
+			dJac := mat.Det(jac)
+			if dJac < 0 {
+				return fmt.Errorf("negative determinant of jacobian of element #%d, Check node ordering", iele)
+			} else if dJac < 1e-12 {
+				return fmt.Errorf("zero determinant of jacobian of element #%d, Check element shape for bad aspect ratio", iele)
+			}
+			err := dNxy.Solve(jac, dN)
+			if err != nil {
+				return fmt.Errorf("error calculating element #%d form factor: %s", iele, err)
+			}
+			scale := c.SetStrainDisplacementMatrix(B, elemNod, dNxy, Npg[ipg])
+			if math.IsNaN(scale) {
+				return fmt.Errorf("NaN scale value returned by SetStrainDisplacementMatrix at element #%d, quad %d", iele, ipg)
+			}
+			vd := pgStrain.RowView(ipg).(*mat.VecDense)
+			vd.MulVec(B, lapvec{lap.SliceVec(displacements, elemDofs)})
+		}
+		strainCallback(iele, pgStrain.RawMatrix().Data)
+		return nil
+	})
+	return err
+}
+
+type lapvec struct {
+	lap.Vector
+}
+
+func (v lapvec) T() mat.Matrix {
+	return lapmat{lap.T(v.Vector)}
+}
--- constitution/solids/patch_test.go
+++ constitution/solids/patch_test.go
@@ -1,6 +1,7 @@
 package solids_test
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 
@@ -12,10 +13,17 @@ import (
 	"gonum.org/v1/gonum/spatial/r3"
 )
 
-func TestPatchAxisymmetric(t *testing.T) {
+// See [1.5.4] Patch test for axisymmetric elements.
+//
+// [1.5.4]: https://classes.engineering.wustl.edu/2009/spring/mase5513/abaqus/docs/v6.5/books/ver/default.htm
+func TestAxisymmetricPatchTest(t *testing.T) {
 	const (
 		// Washer dimensions. ri=internal radius; lx=washer radial width; ly=washer height.
-		ri, lx, ly = 1, 0.24, 0.12
+		ri, lx, ly       = 1, 0.24, 0.12
+		relativeError    = 3e-2 // 3% relative error max permissible.
+		case1Strain      = 1e-3
+		case2Strain      = 9.95e-3
+		case2ShearStrain = 0.0
 	)
 	rng := rand.New(rand.NewSource(1))
 	randf := func() float64 { return rng.Float64() }
@@ -40,7 +48,7 @@ func TestPatchAxisymmetric(t *testing.T) {
 		{0, 4, 7, 3},
 		{4, 5, 6, 7},
 	}
-	// getelement := func(i int) (elem []int) { return q4elems[i][:] }
+
 	getelementR3 := func(i int) (elem []int, xC r3.Vec, yC r3.Vec) { return q4elems[i][:], r3.Vec{}, r3.Vec{} }
 	material := solids.Isotropic{E: 1e6, Poisson: 0.25}
 	elemtype := elements.Quad4{}
@@ -51,35 +59,99 @@ func TestPatchAxisymmetric(t *testing.T) {
 	}
 	displacements := lap.NewDenseVector(ga.TotalDofs(), nil)
 	fix := fem.NewFixity(fem.DofPosX|fem.DofPosY, len(nodes))
-	// Displacement loads for case 1.
-	for i, node := range nodes {
-		isTopNode := node.Y == ly
-		isBottomNode := node.Y == 0
-		isLeftNode := node.X == ri
-		isRightNode := node.X == ri+lx
-		isExternalNode := isTopNode || isBottomNode || isLeftNode || isRightNode
-		if isExternalNode {
-			displacements.SetVec(i*2, node.X/1000)
-			displacements.SetVec(i*2+1, (node.Y+node.X)/1000)
-			fix.Fix(i, fem.DofPosX|fem.DofPosY)
+
+	t.Run("Case 1", func(t *testing.T) {
+		// Displacement loads for case 1.
+		for i, node := range nodes {
+			isTopNode := node.Y == ly
+			isBottomNode := node.Y == 0
+			isLeftNode := node.X == ri
+			isRightNode := node.X == ri+lx
+			isExternalNode := isTopNode || isBottomNode || isLeftNode || isRightNode
+			if isExternalNode {
+				displacements.SetVec(i*2, node.X/1000)
+				displacements.SetVec(i*2+1, (node.Y+node.X)/1000)
+				fix.Fix(i, fem.DofPosX|fem.DofPosY)
+			}
 		}
-	}
-	free := fix.FreeDofs()
-	fixed := fix.FixedDofs()
-	loadsFromDispl := lap.NewDenseVector(len(free), nil)
-	loadsFromDispl.MulVec(lap.Slice(ga.Ksolid(), free, fixed), lap.SliceVec(displacements, fixed))
+		free := fix.FreeDofs()
+		fixed := fix.FixedDofs()
+		loadsFromDispl := lap.NewDenseVector(len(free), nil)
+		loadsFromDispl.MulVec(lap.Slice(ga.Ksolid(), free, fixed), lap.SliceVec(displacements, fixed))
 
-	// 0 imposed initial imposed loads.  K(free,free)\(R(free)-K(free,fix)*D(fix))
-	imposedLoads := lap.NewDenseVector(len(free), nil)
-	imposedLoads.SubVec(imposedLoads, loadsFromDispl)
+		// 0 imposed initial imposed loads.  K(free,free)\(R(free)-K(free,fix)*D(fix))
+		imposedLoads := lap.NewDenseVector(len(free), nil)
+		imposedLoads.SubVec(imposedLoads, loadsFromDispl)
 
-	resultDisplacements := mat.NewVecDense(len(free), nil)
-	resultDisplacements.SolveVec(lapmat{lap.Slice(ga.Ksolid(), free, free)}, lapvec{imposedLoads})
-	// Extract the solution to obtain all displacements, both fixed and free.
-	lap.SliceVec(displacements, free).CopyVec(resultDisplacements)
-	// Compute Reaction forces.
-	forces := lap.NewDenseVector(ga.TotalDofs(), nil)
-	forces.MulVec(ga.Ksolid(), displacements)
+		resultDisplacements := mat.NewVecDense(len(free), nil)
+		resultDisplacements.SolveVec(lapmat{lap.Slice(ga.Ksolid(), free, free)}, lapvec{imposedLoads})
+		// Extract the solution to obtain all displacements, both fixed and free.
+		lap.SliceVec(displacements, free).CopyVec(resultDisplacements)
+		// Compute Reaction forces.
+		forces := lap.NewDenseVector(ga.TotalDofs(), nil)
+		forces.MulVec(ga.Ksolid(), displacements)
+
+		err = ga.IsoparametricStrains(displacements, elemtype, material.Axisymmetric(), len(q4elems), getelementR3, func(iele int, strains []float64) {
+			for i := range strains {
+				if math.Abs(strains[i]-case1Strain)/case1Strain > relativeError {
+					t.Errorf("expected strain elem#%d to be %g, got %g", iele, case1Strain, strains[i])
+					break
+				}
+			}
+		})
+	})
+
+	// Case 2: Displacement loads for case 2.
+	t.Run("Case 2", func(t *testing.T) {
+		// Displacement loads for case 1.
+		for i, node := range nodes {
+			isTopNode := node.Y == ly
+			isBottomNode := node.Y == 0
+			isLeftNode := node.X == ri
+			isRightNode := node.X == ri+lx
+			isExternalNode := isTopNode || isBottomNode || isLeftNode || isRightNode
+			if isExternalNode {
+				displacements.SetVec(i*2, node.X/100)
+				displacements.SetVec(i*2+1, (node.Y)/100)
+				fix.Fix(i, fem.DofPosX|fem.DofPosY)
+			}
+		}
+		free := fix.FreeDofs()
+		fixed := fix.FixedDofs()
+		loadsFromDispl := lap.NewDenseVector(len(free), nil)
+		loadsFromDispl.MulVec(lap.Slice(ga.Ksolid(), free, fixed), lap.SliceVec(displacements, fixed))
+
+		// 0 imposed initial imposed loads.  K(free,free)\(R(free)-K(free,fix)*D(fix))
+		imposedLoads := lap.NewDenseVector(len(free), nil)
+		imposedLoads.SubVec(imposedLoads, loadsFromDispl)
+
+		resultDisplacements := mat.NewVecDense(len(free), nil)
+		resultDisplacements.SolveVec(lapmat{lap.Slice(ga.Ksolid(), free, free)}, lapvec{imposedLoads})
+		// Extract the solution to obtain all displacements, both fixed and free.
+		lap.SliceVec(displacements, free).CopyVec(resultDisplacements)
+		// Compute Reaction forces.
+		forces := lap.NewDenseVector(ga.TotalDofs(), nil)
+		forces.MulVec(ga.Ksolid(), displacements)
+
+		err = ga.IsoparametricStrains(displacements, elemtype, material.Axisymmetric(), len(q4elems), getelementR3, func(iele int, strains []float64) {
+			for i := 0; i < len(strains); i += 4 {
+				strainRadial := strains[i]
+				strainZ := strains[i+1]
+				strainTheta := strains[i+2]
+				strainShear := strains[i+3]
+				switch {
+				case math.Abs(strainRadial-case2Strain)/case2Strain > relativeError:
+					t.Errorf("expected radial strain elem#%d to be %g, got %g", iele, case2Strain, strainRadial)
+				case math.Abs(strainZ-case2Strain)/case2Strain > relativeError:
+					t.Errorf("expected Z strain elem#%d to be %g, got %g", iele, case2Strain, strainZ)
+				case math.Abs(strainTheta-case2Strain)/case2Strain > relativeError:
+					t.Errorf("expected Theta strain elem#%d to be %g, got %g", iele, case2Strain, strainTheta)
+				case math.Abs(strainShear-case2ShearStrain) > 1e-8:
+					t.Errorf("expected Shear strain elem#%d to be %g, got %g", iele, case2ShearStrain, strainShear)
+				}
+			}
+		})
+	})
 }
 
 type lapvec struct {
